1e9f24fe08e7f72dafaa5d30dbade70b4d5c9572
JEZHOU99 <81694589+Je-Zhou@users.noreply.github.com>
2024-02-28 23:27:36 +1100

    Average Order Value - Done

--- average-order-value [✅]/test_data.sql
+++ average-order-value [✅]/test_data.sql
@@ -13,3 +13,34 @@ insert into orders (order_id,customer_id,ordered_at) values (10248,3,'1996-07-04
 insert into products (product_id,name) values (11,'Rambutan'),(42,'The Matrix Reloaded'),(72,'Milk (2 litres)'),(14,'QZ-183'),(51,'Oak Plank (2m x 4m)'),(31,'Loot Crate'),(39,'F-111 Aardvark'),(49,'Fresh Air'),(71,'Bowler Hat'),(24,'12 month subscription'),(59,'Bunch of Roses') ;
 
 insert into order_line_items (order_line_item_id,order_id,product_id,unit_price,quantity) values (24601,10248,11,14,12),(24602,10248,42,9,10),(24603,10248,72,34,5),(24604,10249,14,18,9),(24605,10249,51,42,40),(24606,10253,31,10,20),(24607,10253,39,14,42),(24608,10253,49,16,40),(24609,10274,71,17,20),(24610,10274,72,27,7),(24611,10275,24,3,12),(24612,10275,59,44,6),(24613,10296,11,16,12),(24614,10296,51,13,30),(24615,10296,39,28,15),(24616,10275,72,3,3),(24617,10275,39,50,4),(24618,10275,11,4,5) ;
+
+SELECT
+  name,
+  COUNT(order_id) as num_orders,
+  SUM(order_value) as total_order_value,
+  AVG(order_value) as avg_order_value
+
+FROM 
+  customers
+JOIN
+(
+  SELECT 
+    orders.order_id as order_id,
+    customer_id,
+    SUM(order_line_items.unit_price * order_line_items.quantity) as order_value
+  FROM
+    orders
+  JOIN
+    order_line_items ON orders.order_id = order_line_items.order_id
+  GROUP BY
+    orders.order_id
+) orderSum
+
+ON
+  orderSum.customer_id = customers.customer_id
+
+GROUP BY
+  name
+  
+ORDER BY
+  avg_order_value DESC;
